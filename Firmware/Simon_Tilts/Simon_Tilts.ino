0951ca752174e5507eeee0122c89a5b62e87c61b
Pete Lewis <pete.lewis@sparkfun.com>
2014-01-13 09:49:43 -0700

    line widths and more
    -Reduced remaining line widths of comments
    -Fixed comment about tilt sensor explanation (impossibilities) to 1 and
    6, (not 5 and 6)
    -changes comparisons to fail (a boolean variable ) to false/true rather
    than "0" and "1"

--- Firmware/Simon_Tilts/Simon_Tilts.ino
+++ Firmware/Simon_Tilts/Simon_Tilts.ino
@@ -25,7 +25,7 @@
 #include "pitches.h" // This header file defines the values needed to make 
                      // "scaled" tones on the buzzer
 
-//-------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // HARDWARE PIN DECLARATIONS
 
 // Define an array to include the pins for all six LEDs.
@@ -50,7 +50,7 @@ const int led_gnd_pin = 10;
   // sensor. See explanation below for more info.
 const int tilt_pin[3] = {14,15,16};
 
-//-------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // GAMEPLAY VARIABLE DECLARATIONS
 
 // Define an array to include six NOTES that will be used during gameplay             
@@ -71,7 +71,7 @@ boolean waiting_mode = true;
   // "False" means there is no failure, we will initialize it this way
 boolean fail = false;
                   
-//-------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // LEVEL SPECIFIC VARIABLE DECLARATIONS
 
 // Define a variable to keep track of which level the player is in
@@ -127,7 +127,7 @@ int game_length = 0;
   // See lookup_possible_positions() for more info.
 int next_possible_positions[4] = {};
 
-//-------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // TILT SENSOR SPECIFIC VARIABLE DECLARATIONS
 
 // Infrared tilt sensor explanation
@@ -155,9 +155,9 @@ int next_possible_positions[4] = {};
 //   BACK         0              1              1                 3
 
 // Define an array of "good" readings from the tilt sensors
-  // Notice how 5 and 6 are not included as possibilities in the table above.
+  // Notice how 1 and 6 are not included as possibilities in the table above.
   // This is because of the particular angle and placement of the tilt arms.
-  // 5 and 6 are impossible, so in an effort to debounce the data, we create the 
+  // 1 and 6 are impossible, so in an effort to debounce the data, we create the 
   // following array. The order of this array is important, because it 
   // corresponds with the led_pin[] array.
 const byte correct_tilt_byte[6] = {7,4,5,3,2,0};
@@ -233,7 +233,7 @@ void loop()
   set_level_variables(level); // this adjusts moves_to_win, game_length, 
                               // and playback_delay_time
   
-  if(fail == 1){
+  if(fail == true){
     Serial.println("Starting Over...");
     randomSeed(analogRead(A7));
     fail = 0;
@@ -383,12 +383,16 @@ void listen_for_pattern()
     {
       read_position_w_debounce(); // Take in a reading of position with debounce
       
-      if(current_tilt_position == game_pattern[i]) // (1) Are they on the correct next position?
+      // (1) Are they on the correct next position?
+      if(current_tilt_position == game_pattern[i]) 
         {
-          blink_led_w_fade(current_tilt_position); // Blink current position once to indicate they did it correctly
+          // Blink current position once to indicate they did it correctly
+          blink_led_w_fade(current_tilt_position); 
           break;
         }
-      else if ((timeout == 49)) // (2) how long have they been trying (or thinking), is it time to timeout?
+        
+      // (2) how long have they been trying (or thinking), is it time to timeout?
+      else if ((timeout == 49)) 
       {
         Serial.print("Timed out on position ");
         Serial.println(i);
@@ -399,21 +403,31 @@ void listen_for_pattern()
         fail = 1;
         break;
       }
-      else if(current_tilt_position == game_pattern[i-1]); // (3) Are they still on the previous position? If so, do nothing and increment timeout (happens below)
-      else if(current_tilt_position != game_pattern[i]) wrong_positions += 1; // (4) Did the player rotate to the wrong position? 
-                                                                            //  If so, keep track of how long by incrementing "wrong_positions"
       
-      if(wrong_positions > 15) // (5) Has the player stayed on a wrong position for 15 counts?
+      // (3) Are they still on the previous position? 
+      // If so, do nothing and increment timeout (happens below)
+      else if(current_tilt_position == game_pattern[i-1]); 
+      
+      // (4) Did the player rotate to the wrong position? 
+      //  If so, keep track of how long by incrementing "wrong_positions"
+      else if(current_tilt_position != game_pattern[i]) wrong_positions += 1; 
+                                                                            
+      // (5) Has the player stayed on a wrong position for 15 counts?
+      if(wrong_positions > 15) 
       {
         Serial.print("you tumbled wrong:");
         Serial.println(current_tilt_position);
-        blink_led(current_tilt_position, 50); // Blink quickly to show the incorrect tilt position
+        // Blink quickly to show the incorrect tilt position
+        blink_led(current_tilt_position, 50); 
         blink_all(50);
-        blink_led(game_pattern[i], 100); // Blink where the player should have rolled to
+        // Blink where the player should have rolled to
+        blink_led(game_pattern[i], 100); 
         delay(10);
-        blink_led(game_pattern[i], 100); // Blink where the player should have rolled to (again)
+        // Blink where the player should have rolled to (again)
+        blink_led(game_pattern[i], 100); 
         delay(10);
-        blink_led(game_pattern[i], 400); // Blink where the player should have rolled to (and  a 3rd time)
+        // Blink where the player should have rolled to (and  a 3rd time)
+        blink_led(game_pattern[i], 400); 
         game_length = 0; // this gets us out of listen_for_pattern()
         level = 1;
         fail = 1;
@@ -423,7 +437,7 @@ void listen_for_pattern()
       timeout += 1;   // Increment timeout
    } // End of timeout while loop
   
-   if((i == game_length) && (fail == 0))
+   if((i == game_length) && (fail == false))
    {
      Serial.println("current gamelength complete!");
      Serial.println(current_tilt_position);
@@ -437,7 +451,7 @@ void listen_for_pattern()
        level += 1; // go to next level
      }
      else{
-       delay(1000); // wait a sec to give the player time to be ready for playback
+       delay(1000); // delay to give the player time to be ready for playback
      }
     }
   } // end of game pattern for loop
@@ -474,13 +488,13 @@ void blink_led_w_fade(int num)
   // fade out from max to min in increments of 40 points:
   for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=40) 
   {
-    analogWrite(led_gnd_pin, fadeValue); // sets the value (range from 0 to 255):  
+    analogWrite(led_gnd_pin, fadeValue); // sets the value (range from 0-255):  
     delay(30); // wait for 30 milliseconds to see the dimming effect   
   } 
      
   for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=70) 
   { 
-    analogWrite(led_gnd_pin, fadeValue); // sets the value (range from 0 to 255):      
+    analogWrite(led_gnd_pin, fadeValue); // sets the value (range from 0-255):      
     delay(30); // wait for 30 milliseconds to see the dimming effect                           
   } 
   digitalWrite(led_pin[num], LOW);
@@ -508,7 +522,8 @@ void blink_all(int delay_time)
   // This happens when they stay on a position for too long
   // The "timeout" variable is defined in listen_for_pattern()
   // This function plays a low tone and blinks all six LEDs in sequence
-  // It is intended to be very different from winner_blinky() which is kind of "cheery"
+  // It is intended to be very different from winner_blinky() which is kind of 
+  // "cheery"
 void blink_player_timed_out(void)
 {
   tone(12, 150);
@@ -521,10 +536,10 @@ void blink_player_timed_out(void)
 
 
 // Define a function that will show various blinky action and sound buzzing
-  // This is used to control what the player sees and hears when they win each level
-  // It's basically a way to call winner_blinky different amounts of times at different speeds
-  // This way when you win each level, you see a unique display and it "sort of" shows you 
-  // which level you just completed
+  // This is used to control what the player sees and hears when they win each 
+  // level. It's basically a way to call winner_blinky different amounts of 
+  // times at different speeds. This way when you win each level, you see a 
+  // unique display and it sort of shows you which level you just completed.
 void show_winner_blinkies()
 {
   if(level == 1)
@@ -543,16 +558,17 @@ void show_winner_blinkies()
          winner_blinky(400);
       }
   }
-  delay(1000); // This gives the player a second to get ready for the next level
+  delay(1000); // Give the player a second to get ready for the next level
 }
 
 
 // Define a function to blink the six LEDs in sequence, while also playing tones
-  // Send this function an integer, increment_val, to speed up or slow down the sequence
-  // This function is basically comprised of two for loops.
-  // One to blink LEDs and buzz tones that start at low pitches and then climb up to high pitches
-  // And a second to start at high pitches and then decline down to low pitches
-  // It's mostly just a fun thing to see and hear when you complete a level
+  // Send this function an integer, increment_val, to speed up or slow down the 
+  // sequence. This function is basically comprised of two for loops.
+  // One to blink LEDs and buzz tones that start at low pitches and then climb 
+  // up to high pitches. And a second to start at high pitches and then decline 
+  // down to low pitches. It's mostly just a fun thing to see and hear when you 
+  // complete a level.
 void winner_blinky(int increment_val)
 {
   int num = 0; // starting led
@@ -590,11 +606,11 @@ void winner_blinky(int increment_val)
 
 
 // Define a function to show current game pattern to user
-  // This function blinks a sequence of LEDs (and also buzzes each corresponding tone)
-  // It is basically a for loop that starts at the beginning, and then continues through the entire game length
-  // Note, gamelength is incremented in the main loop.
-  // As the player repeats the current "sub" pattern correctly, gamelength increments
-  // This continues on until they win the current level
+  // This function blinks a sequence of LEDs (and also buzzes each corresponding 
+  // tone). It is basically a for loop that starts at the beginning, and then 
+  // continues through the entire game length. Note, gamelength is incremented 
+  // in the main loop. As the player repeats the current "sub" pattern correctly, 
+  // gamelength increments. This continues on until they win the current level.
 void show_game_pattern()
 {
   Serial.print("\n\r\n\rgame_pattern:");
@@ -605,7 +621,8 @@ void show_game_pattern()
     blink_led_w_fade(game_pattern[i]);
     delay(playback_delay_time);
   }
-  Serial.println(" "); // print a new line, this helps keep the debug serial window more legible
+  // print a new line, this helps keep the debug serial window more legible
+  Serial.println(" "); 
 }
 
 
@@ -616,9 +633,9 @@ void show_game_pattern()
   // the BB rests in it's corresponding tilt arm.
   // If we take an analogRead() on one of the infrared detectors, we can know
   // if the BB is present or not (that is, blocking the IR emitter).
-  // Through some testing we found that when the BB is at the bottom of the tilt arm,
-  // and blocking the IR, the analogRead will consistently return a value greater that 875. 
-  // This is our "threshold" value.
+  // Through some testing we found that when the BB is at the bottom of the tilt 
+  // arm, and blocking the IR, the analogRead will consistently return a value 
+  // greater that 875. This is our "threshold" value.
 byte read_position_once()
 {
   byte result = 0;
@@ -631,8 +648,9 @@ byte read_position_once()
 
 // Define a function to set level variables
   // Send this function the current level, which can be any integer >=1
-  // It takes the level and then adjusts the gameplay variables accordingly
-  // This gameplay variables are then used in listen_for_pattern() to control the difficulty of the game
+  // It takes the level and then adjusts the gameplay variables accordingly.
+  // This gameplay variables are then used in listen_for_pattern() to control 
+  // the difficulty of the game.
 void set_level_variables(int level)
 {
   if(level == 1)
@@ -654,28 +672,36 @@ void set_level_variables(int level)
 
 
 // Define a function to be used during "waiting mode"
-  // This function basically blinks al six LEDs and waits for the player to tilt the game in any direction
-  // It reads the current position, and then reads a second time,
-  // and then compares the two readings to know if it's changed.
-  // When this happens, it changes the global variable, "waiting_mode" to false, which ultimately kills waiting mode
+  // This function basically blinks al six LEDs and waits for the player to 
+  // tilt the game in any direction. It reads the current position, and then 
+  // reads a second time. And then compares the two readings to know if it's 
+  // changed. When this happens, it changes the global variable, "waiting_mode" 
+  // to false, which ultimately kills waiting mode.
 void wait_for_user_input_change(void)
 {
-  byte prev_pos = read_position_once(); // store the current position so we can compare it later and notice a change.
+  // store the current position so we can compare it later and notice a change.
+  byte prev_pos = read_position_once(); 
   blink_all(50); // Blink to indicate we're in waiting mode
-  if(prev_pos != read_position_once()) // if the player has rotated, then this new "fresh" reading will be different than "prev_pos"
+  // if the player has rotated, then this new "fresh" reading will be different 
+  // than "prev_pos"
+  if(prev_pos != read_position_once()) 
   {
-    delay(1000); // Delay to allow the user to return to the home position (with the silk "Simon Tilts" reading left to right)
-    waiting_mode = false; // This ultimately will get us out of the waiting mode while loop (at the top of the main loop)
-                          // and then move on to the gameplay (which lives in the remainder of the main loop)
+    delay(1000); // Delay to allow the user to return to the home position 
+                 //(with the silk "Simon Tilts" reading left to right)
+    waiting_mode = false; // This ultimately will get us out of the waiting mode 
+    // while loop (at the top of the main loop) and then move on to the gameplay 
+    // (which lives in the remainder of the main loop)
   }
 }
 
 
 // Define a function to update the next possible positions
   // For use in creating a nice random game pattern
-  // This function acts as a set of "rules" to ensure we don't have two of the same positions in a row for any game pattern
-  // You send it the current position, and it will change the values in the array next_possible_positions[].
-  // The new values will only include positions that are adjacent to the current position
+  // This function acts as a set of "rules" to ensure we don't have two of the 
+  // same positions in a row for any game pattern. You send it the current 
+  // position, and it will change the values in the array 
+  // next_possible_positions[]. The new values will only include positions 
+  // that are adjacent to the current position.
 void lookup_possible_positions(int tilt_pos)
 {
   switch (tilt_pos)
@@ -728,18 +754,20 @@ void lookup_possible_positions(int tilt_pos)
   // These for loops are from ARDUINO>>EXAMPLES>>ANALOG>>FADING
   // the fadeValue increment changes the speed of the fade
 void nightlight_fade(int num){
-  digitalWrite(led_gnd_pin, HIGH); // Turn the GND return HIGH, which effectively turns all LEDs off
+  digitalWrite(led_gnd_pin, HIGH); // Turn the GND return HIGH, 
+                                   // which effectively turns all LEDs off
   digitalWrite(led_pin[num], HIGH); // Turn the LED we'd like to fade on to HIGH
   
   // Fade IN, note because we are using PWM on the GND return to control the fade,
-  // we start at 255 (HIGH - or off) and then slowly decrease to 0 (LOW - full brightness)
+  // we start at 255 (HIGH - or off).
+  // And then slowly decrease to 0 (LOW - full brightness)
   for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=1) { 
     analogWrite(led_gnd_pin, fadeValue); // sets the value (range from 0 to 255):    
     delay(60); // wait for 60 milliseconds to see the dimming effect    
      } 
      
-  // Fade OUT, note because we are using PWM on the GND return to control the fade,
-  // we start at 0 (LOW) and then slowly increase to 255 (HIGH)
+  // Fade OUT, note because we are using PWM on the GND return to control the 
+  // fade, we start at 0 (LOW) and then slowly increase to 255 (HIGH)
   for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=1) { 
     analogWrite(led_gnd_pin, fadeValue);         
     delay(60);                            
